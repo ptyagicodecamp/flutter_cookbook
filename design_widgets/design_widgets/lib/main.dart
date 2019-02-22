c1f47893aaaaf609f3fc87908ad4816f831bea8d
ptyagi <ptyagicodecamp@gmail.com>
2019-02-22 11:19:30 -0800

    added routenames to navigate expansiontiles

--- design_widgets/design_widgets/lib/main.dart
+++ design_widgets/design_widgets/lib/main.dart
@@ -2,26 +2,6 @@ import 'package:design_widgets/snackbar.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
-
-class RecipeEntry {
-  final String title;
-  final List<RecipeEntry> children;
-  const RecipeEntry(this.title, [this.children = const <RecipeEntry>[]]);
-}
-
-const List<RecipeEntry> entries = <RecipeEntry>[
-  RecipeEntry(
-    'Snackbar'
-  ),
-  RecipeEntry(
-    'Sliver AppBar',
-    <RecipeEntry>[
-      RecipeEntry('Floating SliverAppBar'),
-      RecipeEntry('Pinned SliverAppBar'),
-      RecipeEntry('Snap SliverAppBar')
-    ],
-  ),
-];
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -32,9 +12,42 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: DesignWidgetsRecipes(title: 'Design Widgets Recipes'),
+      initialRoute: '/',
+      routes: {
+        // When we navigate to the "/" route, build the DesignWidgetsRecipes Widget
+       // '/': (context) => DesignWidgetsRecipes(),
+        '/snackbar': (context) => SnackbarRecipe(),
+        '/sliver_floating': (context) => SnackbarRecipe(),
+        '/sliver_pinned': (context) => SnackbarRecipe(),
+        '/sliver_snap': (context) => SnackbarRecipe(),
+
+      },
     );
   }
 }
+
+class RecipeEntry {
+  final String key;
+  final String title;
+  final List<RecipeEntry> children;
+  const RecipeEntry(this.key, this.title,
+      [this.children = const <RecipeEntry>[]]);
+}
+
+const List<RecipeEntry> entries = <RecipeEntry>[
+  RecipeEntry('snackbar', 'Snackbar'),
+  RecipeEntry(
+    '/',
+    'Sliver AppBar',
+    <RecipeEntry>[
+      RecipeEntry('sliver_floating', 'Floating Sliver AppBar'),
+      RecipeEntry('sliver_pinned', 'Pinned Sliver AppBar'),
+      RecipeEntry('sliver_snap', 'Snap Sliver AppBar')
+    ],
+  ),
+];
+
+
 
 class DesignWidgetsRecipes extends StatefulWidget {
   DesignWidgetsRecipes({Key key, this.title}) : super(key: key);
@@ -54,44 +67,43 @@ class _DesignWidgetsRecipesState extends State<DesignWidgetsRecipes> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        child: buildListView(context),
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index) =>
+            RecipeEntryItem(context, entries[index]),
+        itemCount: entries.length,
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
-  Widget buildListView(BuildContext context) {
-    var listTiles = <Widget>[
-      ListTile(
-        title: const Text('Silver AppBar'),
-        subtitle: const Text('An auto-hide AppBar'),
-        leading: const Icon(Icons.flare),
-        onTap: () {
-          //buildSnackbar(context, '', 'Silverbar Demo');
-        },
-      ),
-      Divider(),
-      ListTile(
-        title: const Text('Snackbar'),
-        subtitle:
-            const Text('Widget to show status and take in-context action'),
-        leading: const Icon(Icons.message),
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => SnackbarRecipe()),
-          );
-        },
-      ),
-    ];
-
-    return ListView(
-      children: listTiles,
-    );
-  }
-
-
+//  Widget buildListView(BuildContext context) {
+//    var listTiles = <Widget>[
+//      ListTile(
+//        title: const Text('Sliver AppBar'),
+//        subtitle: const Text('An auto-hide AppBar'),
+//        leading: const Icon(Icons.flare),
+//        onTap: () {
+//          //buildSnackbar(context, '', 'Silverbar Demo');
+//        },
+//      ),
+//      Divider(),
+//      ListTile(
+//        title: const Text('Snackbar'),
+//        subtitle:
+//            const Text('Widget to show status and take in-context action'),
+//        leading: const Icon(Icons.message),
+//        onTap: () {
+//          Navigator.push(
+//            context,
+//            MaterialPageRoute(builder: (context) => SnackbarRecipe()),
+//          );
+//        },
+//      ),
+//    ];
+//
+//    return ListView(
+//      children: listTiles,
+//    );
+//  }
 }
 
 class ListItem {
@@ -104,3 +116,36 @@ class ListItem {
   }
 }
 
+// Displays one RecipeEntry. RecipeEntry's children are shown in ExpansionTile (s).
+class RecipeEntryItem extends StatelessWidget {
+  const RecipeEntryItem(this.context, this.entry);
+
+  final RecipeEntry entry;
+  final BuildContext context;
+
+  Widget _buildListTiles(RecipeEntry root) {
+    if (root.children.isEmpty)
+      return ListTile(
+        title: Text(root.title),
+        onTap: () {
+          Navigator.pushNamed(context, "/" + root.key);
+        },
+      );
+    return ExpansionTile(
+      key: PageStorageKey<RecipeEntry>(root),
+      title: Text(root.title),
+      children: root.children.map(_buildListTiles).toList(),
+    );
+  }
+
+  navigateToRecipe() {
+    Navigator.of(this.context).push(new MaterialPageRoute(builder: (context) {
+      return new SnackbarRecipe();
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildListTiles(entry);
+  }
+}
