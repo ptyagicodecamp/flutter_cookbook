dee1123033ab2a7aa020b4efaefe6a360f79f8ee
ptyagicodecamp <ptyagicodecamp@gmail.com>
2019-11-23 22:07:19 -0800

    build

---
+++ search_appbar/search_appbar/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/ptyagi/fluttersdk/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ptyagi/Developer/ptyagicodecamp/flutter_cookbook/search_appbar/search_appbar"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/ptyagi/fluttersdk/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
