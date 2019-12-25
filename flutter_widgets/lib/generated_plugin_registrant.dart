15f2999f168f00b0416b9a1c3242cd92663362f3
ptyagicodecamp <ptyagicodecamp@gmail.com>
2019-12-24 20:50:28 -0800

    merged web

---
+++ flutter_widgets/lib/generated_plugin_registrant.dart
@@ -0,0 +1,15 @@
+//
+// Generated file. Do not edit.
+//
+import 'dart:ui';
+
+import 'package:google_sign_in_web/google_sign_in_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins(PluginRegistry registry) {
+  GoogleSignInPlugin.registerWith(registry.registrarFor(GoogleSignInPlugin));
+  UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));
+  registry.registerMessageHandler();
+}
--- flutter_widgets/lib/login/user_profile.dart
+++ flutter_widgets/lib/login/user_profile.dart
@@ -188,4 +188,3 @@ class _UserProfilePageState extends State<UserProfilePage> {
 //    );
 //  }
 //}
->>>>>>> widgets-web
