fc68047d4a161f734c228b2d273ce5ef0d3055ab
ptyagicodecamp <ptyagicodecamp@gmail.com>
2020-03-29 22:49:19 -0700

    comments added

--- flutter_widgets/lib/canvas/painting.dart
+++ flutter_widgets/lib/canvas/painting.dart
@@ -13,8 +13,7 @@ class CanvasPainting extends StatefulWidget {
 class _CanvasPaintingState extends State<CanvasPainting> {
   List<TouchPoints> points = List();
   double opacity = 1.0;
-  StrokeCap strokeType = StrokeCap
-      .round; //(Platform.isAndroid) ? StrokeCap.butt : StrokeCap.round;
+  StrokeCap strokeType = StrokeCap.round;
   double strokeWidth = 3.0;
   Color selectedColor = Colors.black;
 
@@ -277,12 +276,17 @@ class _CanvasPaintingState extends State<CanvasPainting> {
 
 class MyPainter extends CustomPainter {
   MyPainter({this.pointsList});
+
+  //Keep track of the points tapped on the screen
   List<TouchPoints> pointsList;
   List<Offset> offsetPoints = List();
+
+  //This is where we can draw on canvas.
   @override
   void paint(Canvas canvas, Size size) {
     for (int i = 0; i < pointsList.length - 1; i++) {
       if (pointsList[i] != null && pointsList[i + 1] != null) {
+        //Drawing line when two consecutive points are available
         canvas.drawLine(pointsList[i].points, pointsList[i + 1].points,
             pointsList[i].paint);
       } else if (pointsList[i] != null && pointsList[i + 1] == null) {
@@ -290,15 +294,20 @@ class MyPainter extends CustomPainter {
         offsetPoints.add(pointsList[i].points);
         offsetPoints.add(Offset(
             pointsList[i].points.dx + 0.1, pointsList[i].points.dy + 0.1));
+
+        //Draw points when two points are not next to each other
         canvas.drawPoints(PointMode.points, offsetPoints, pointsList[i].paint);
       }
     }
   }
 
+  //Called when CustomPainter is rebuilt.
+  //Returning true because we want canvas to be rebuilt to reflect new changes.
   @override
   bool shouldRepaint(MyPainter oldDelegate) => true;
 }
 
+//Class to define a point touched at canvas
 class TouchPoints {
   Paint paint;
   Offset points;
