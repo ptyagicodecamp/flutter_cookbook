8ab8280e7f67e9486e5c51262b49480a665cb1a1
ptyagicodecamp <ptyagicodecamp@gmail.com>
2020-03-31 23:56:14 -0700

    updated for flutter beta

--- flutter_widgets/lib/canvas/painting.dart
+++ flutter_widgets/lib/canvas/painting.dart
@@ -3,7 +3,7 @@ import 'dart:ui';
 import 'package:animated_floatactionbuttons/animated_floatactionbuttons.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(CanvasPainting());
+//void main() => runApp(CanvasPainting());
 
 class CanvasPainting extends StatefulWidget {
   @override
@@ -18,13 +18,19 @@ class _CanvasPaintingState extends State<CanvasPainting> {
   Color selectedColor = Colors.black;
 
   Future<void> _pickStroke() async {
+    //Shows AlertDialog
     return showDialog<void>(
       context: context,
+
+      //Dismiss alert dialog when set true
       barrierDismissible: true, // user must tap button!
       builder: (BuildContext context) {
+        //Clips its child in a oval shape
         return ClipOval(
           child: AlertDialog(
+            //Creates three buttons to pick stroke value.
             actions: <Widget>[
+              //Resetting to default stroke value
               FlatButton(
                 child: Icon(
                   Icons.clear,
@@ -72,12 +78,18 @@ class _CanvasPaintingState extends State<CanvasPainting> {
   }
 
   Future<void> _opacity() async {
+    //Shows AlertDialog
     return showDialog<void>(
       context: context,
-      barrierDismissible: true, // user must tap button!
+
+      //Dismiss alert dialog when set true
+      barrierDismissible: true,
+
       builder: (BuildContext context) {
+        //Clips its child in a oval shape
         return ClipOval(
           child: AlertDialog(
+            //Creates three buttons to pick opacity value.
             actions: <Widget>[
               FlatButton(
                 child: Icon(
@@ -85,6 +97,7 @@ class _CanvasPaintingState extends State<CanvasPainting> {
                   size: 24,
                 ),
                 onPressed: () {
+                  //most transparent
                   opacity = 0.1;
                   Navigator.of(context).pop();
                 },
@@ -105,6 +118,7 @@ class _CanvasPaintingState extends State<CanvasPainting> {
                   size: 60,
                 ),
                 onPressed: () {
+                  //not transparent at all.
                   opacity = 1.0;
                   Navigator.of(context).pop();
                 },
--- flutter_widgets/lib/generated_plugin_registrant.dart
+++ flutter_widgets/lib/generated_plugin_registrant.dart
@@ -11,7 +11,6 @@ import 'package:url_launcher_web/url_launcher_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
-// ignore: public_member_api_docs
 void registerPlugins(PluginRegistry registry) {
   GoogleSignInPlugin.registerWith(registry.registrarFor(GoogleSignInPlugin));
   SharedPreferencesPlugin.registerWith(registry.registrarFor(SharedPreferencesPlugin));
